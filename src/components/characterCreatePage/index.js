afdbdeb66bcd394e37fd09fd4cc5610e209e4301
Pongo <andrew.rodiek@gmail.com>
2021-12-02 22:27:15 -0500

    Playing around with attacks

--- src/components/characterCreatePage/index.js
+++ src/components/characterCreatePage/index.js
@@ -13,7 +13,7 @@ class CharacterCreatePage extends Component {
       level: 1,
       class: '',
       subclass: '',
-      proficiency: 0,
+      proficiency: 2,
       abilities: {
         strength: {name: "Strength", val: 10, save: false, id:"158a7750-126a-4389-ab4a-c247cb09b783"},
         dexterity: {name: "Dexterity", val: 10, save: false, id:"fffadc2d-e76e-4b1a-adc6-3b07ee29e40a"},
@@ -57,11 +57,32 @@ class CharacterCreatePage extends Component {
     }
   };
 
-  handleSkillButtonClick = (key, buttonLabel) => {
+  handleProfSkillButtonClick = (key) => {
+    if(this.state.skills[key].prof && this.state.skills[key].exp){
+      this.setState({ 
+        skills: { ...this.state.skills, 
+          [key]: {...this.state.skills[key],
+            prof: !this.state.skills[key].prof,
+            exp: false
+          }
+        }
+      })
+      return;
+    }
     this.setState({ 
       skills: { ...this.state.skills, 
         [key]: {...this.state.skills[key],
-          [buttonLabel]: !this.state.skills[key][buttonLabel]
+          prof: !this.state.skills[key].prof
+        }
+      }
+    })
+  };
+
+  handleExpSkillButtonClick = (key) => {
+    this.setState({ 
+      skills: { ...this.state.skills, 
+        [key]: {...this.state.skills[key],
+          exp: !this.state.skills[key].exp
         }
       }
     })
@@ -108,16 +129,54 @@ class CharacterCreatePage extends Component {
   };
 
   deriveSkillValue = (prof, exp, ability) => {
+    const abilityValue = this.calculateAbilityModifier(this.state.abilities[ability].val);
     if(prof) {
-      if(exp) {
-        return this.calculateAbilityModifier(this.state.abilities[ability].val) + this.state.proficiency * 2;
-      } else {
-        return this.calculateAbilityModifier(this.state.abilities[ability].val) + this.state.proficiency;
-      }
+      console.log(`${this.state.proficiency}`);
+      return exp ? (abilityValue + this.state.proficiency * 2) : (abilityValue + this.state.proficiency);
     }
-    return this.calculateAbilityModifier(this.state.abilities[ability].val);
+    return abilityValue;
+  };
+
+  getUniqueIDForAttack = () => {
+    const newVal = Math.floor(Math.random() * 10000);
+    return this.state.attacks.every(attack => attack.id !== newVal) ? newVal : this.getUniqueIDForAttack();
+  };
+
+  // baseline stats. May add others.
+  handleAttackAdd = () => {
+    this.setState({
+      attacks: [...this.state.attacks, {
+        id: this.getUniqueIDForAttack(),
+        name: '', 
+        attackStat: '',
+        proficiency: false,
+        damageDice: '',
+      }]
+    })
+  };
+
+  handleAttackRemove = (removeID) => {
+    const newAttacks = this.state.attacks.filter((attack) => {
+      console.log(attack.id);
+      console.log(removeID);
+      return attack.id !== removeID;
+    });
+    
+    this.setState({
+      attacks: newAttacks
+    })
   };
   
+  //So this probably isn't the best way to handle this
+  handleAttackNameChange = (id, value) => {
+    const newAttacks = this.state?.attacks.map(attack => {
+      return attack.id === id ? {...attack, name: value} : attack;
+    });
+
+    this.setState({
+      attacks: newAttacks
+    });
+  }
 
   render() {
     const {abilities, skills, level} = this.state;
@@ -161,8 +220,8 @@ class CharacterCreatePage extends Component {
                 <div className="skillEditor" key={`${skill.name}-${skill.ability}`}>
                     <h4>{skill.name}: </h4>
                     <p>{this.deriveSkillValue(skill.prof, skill.exp, skill.ability)}</p>
-                    <button className={skill.prof ? 'clicked' : ''} type="button" onClick={() => this.handleSkillButtonClick(index, 'prof')}>Proficiency</button>
-                    {skill.prof && <button className={skill.exp ? 'clicked' : ''} type="button" onClick={() => this.handleSkillButtonClick(index, 'exp')}>Expertise</button>}
+                    <button className={skill.prof ? 'clicked' : ''} type="button" onClick={() => this.handleProfSkillButtonClick(index, 'prof')}>Proficiency</button>
+                    {skill.prof && <button className={skill.exp ? 'clicked' : ''} type="button" onClick={() => this.handleExpSkillButtonClick(index, 'exp')}>Expertise</button>}
                 </div>
               );
             }) }
@@ -171,6 +230,26 @@ class CharacterCreatePage extends Component {
           <h3>Saves: </h3>
           { 
           }
+        </div>
+        <div className="attacks" >
+          <h3>Attacks!: </h3>
+          { 
+            this.state.attacks.map((attack, index) => {
+              return (
+                <div key={`${attack.name}-${attack.attackStat}`} >
+                  <h4>Attack #{index}</h4>
+                  {/* <input type="text" value={attack.name} onChange={({target}) => this.handleAttackNameChange(attack.id, target.value)} />
+                  <div className="attackDamage" >
+                    <label>Damage:</label>
+                    <input type="text" value={attack.damageDice} />
+                  </div> 
+                  <button type="button" className={attack.proficiency ? 'clicked' : ''} >Proficient</button> */}
+                  <button type="button" onClick={() => this.handleAttackRemove(attack.id)}>Remove</button>
+                </div>
+              );
+            })
+          }
+          <button type="button" onClick={this.handleAttackAdd} >Add Attaack!</button>
         </div>
       <input type="submit" value="Submit" />
       </form>
